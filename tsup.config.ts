50e6acc2ac5d2de5d3005c8448f3261f17323645
Emmanuel Vazquez <maanuvazquez@gmail.com>
2023-03-03 17:42:43 -0300

    fix(project): declaration files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,21 @@
 import type { Options } from 'tsup'
 
+const env = process.env.NODE_ENV
+
 const config: Options = {
   entry: ['src/index.ts'],
   dts: true,
   sourcemap: true,
-  format: ['iife', 'cjs', 'esm'],
-  minify: true
+  format: ['cjs', 'esm'],
+  minify: true,
+  splitting: true,
+  clean: true, // clean up the dist folder
+  bundle: true,
+  skipNodeModulesBundle: true,
+  entryPoints: ['src/index.ts'],
+  watch: env === 'development',
+  target: 'es2020',
+  outDir: 'dist'
 }
 
 export default config
